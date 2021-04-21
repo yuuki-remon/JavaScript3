85fa57540223fdc7c2c19bb82e5c29dbc1a9964d
高柳祐貴 <takayanagiyuuki@takayanagiyuukinoMacBook-Pro.local>
2021-04-22 01:30:44 +0900

    再提出用JavaScript3修正済

--- js/main.js
+++ js/main.js
@@ -1,42 +1,37 @@
 {
   const addBtn = document.getElementById('add');
   const todoTask = document.getElementById('txt');
+  const tbody = document.querySelector('tbody');
   
-  let num = 0;
-
-  function addTask() {
-    const tr = document.createElement('tr');
-    
-    id(tr);
-    comment(tr);
-    workingBtn(tr);
-    trashBtn(tr);
-    
-    document.querySelector('tbody').appendChild(tr);
+  const todos = [];
+  
+  function displayTodos() {
+    todos.forEach((todo, index) =>{
+      const tr = document.createElement('tr');
+      const idTd = document.createElement('td');
+      const taskTd = document.createElement('td');
+      const workingTd = document.createElement('td');
+      const input = document.createElement('input');
+      
+      idTd.textContent = index;
+      tr.appendChild(idTd);
+      
+      taskTd.textContent =  todo.task;
+      tr.appendChild(taskTd);
+      console.log(todo.task);
+      
+      input.type = 'button';
+      input.value = todo.status;
+      workingTd.appendChild(input);
+      tr.appendChild(workingTd);
+      
+      createDeleteBtn(tr);
+      
+      tbody.appendChild(tr);
+    });
   }
-  
-  function id(tr) {
-    const td = document.createElement('td');
-    td.textContent = num;
-    tr.appendChild(td);
-  } 
-
-  function comment(tr) {
-    const td = document.createElement('td');
-    td.textContent = todoTask.value;
-    tr.appendChild(td);
-  }  
-
-  function workingBtn(tr) {
-    const td = document.createElement('td');
-    const working = document.createElement('input');
-    working.type = 'button';
-    working.value = '作業中';
-    td.appendChild(working);
-    tr.appendChild(td);
-  }
-  
-  function trashBtn(tr) {
+    
+  function createDeleteBtn(tr) {
     const td = document.createElement('td');
     const trash = document.createElement('input');
     trash.type = 'button';
@@ -44,16 +39,25 @@
     td.appendChild(trash);
     tr.appendChild(td);
   }
-
+    
   addBtn.addEventListener('click', () => {
+    let todo = {
+      task: todoTask.value,
+      status: '作業中',    
+    }
     if (!todoTask.value) {
       alert('入力してください');
       return;
     }
     
-    addTask();
+    todos.push(todo);
+    
+    while(tbody.firstChild) {
+      tbody.removeChild(tbody.firstChild);
+    }
+    
+    displayTodos();
     
     todoTask.value = "";
-    num++;
   });
 }

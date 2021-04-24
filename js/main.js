2f77725ffe65804edb1a894238ad09c4926cf17e
高柳祐貴 <takayanagiyuuki@takayanagiyuukinoMacBook-Pro.local>
2021-04-24 16:52:07 +0900

    JavaScript③-3提出用実装済み

--- js/main.js
+++ js/main.js
@@ -10,8 +10,6 @@
       const tr = document.createElement('tr');
       const idTd = document.createElement('td');
       const taskTd = document.createElement('td');
-      const statusTd = document.createElement('td');
-      const input = document.createElement('input');
       
       idTd.textContent = index;
       tr.appendChild(idTd);
@@ -19,15 +17,32 @@
       taskTd.textContent =  todo.task;
       tr.appendChild(taskTd);
       
-      input.type = 'button';
-      input.value = todo.status;
-      statusTd.appendChild(input);
-      tr.appendChild(statusTd);
+      createStatusBtn(tr, todo);
       
       createTrashBtn(tr, index);
       
       tbody.appendChild(tr);
     });
+  }
+  
+  function createStatusBtn(tr, todo) {
+    const input = document.createElement('input');
+    input.type = 'button';
+    input.value = todo.status;
+    
+    const statusTd = document.createElement('td');
+    statusTd.appendChild(input);
+    
+    statusTd.addEventListener('click', () => {
+      if(todo.status === '作業中') {
+        todo.status = '完了';
+        input.value = todo.status;
+      } else if (todo.status === '完了') {
+        todo.status = '作業中';
+        input.value = todo.status;
+      }
+    });
+    tr.appendChild(statusTd);
   }
     
   function createTrashBtn(tr, index) {
